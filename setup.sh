d3b99b2cf422c924c84e95e4e2bc06ceb50defca
medpaf <medpaf@outlook.com>
2021-07-30 13:52:36 +0800

    added setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,7 @@ sudo apt-get install python3-pip
 sudo pip3 install python-nmap
 
 #Add main .py file to the system path (later)
+cp *.py /usr/bin
+cp methods/*.py /usr/bin
+mv /usr/bin/vbnets.py /usr/bin/vbnets
+chmod 755 /usr/bin/vbnets
