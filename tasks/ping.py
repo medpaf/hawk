b88ed78b6693cce6c3dbef7ec87c1f54c52114ff
medpaf <pafmed@outlook.com>
2021-10-02 10:36:12 +0100

    small updts

--- tasks/ping.py
+++ tasks/ping.py
@@ -3,3 +3,5 @@ import os
 def ping(host):
     print('Ping will start. Press CTRL + C to cancel.')
     os.system(f'ping {host}')
+
+
--- tasks/traceroute.py
+++ tasks/traceroute.py
@@ -3,3 +3,4 @@ import os
 def traceroute(host):
     print('Traceroute will start. Press CTRL + C to cancel.')
     os.system(f'traceroute {host}')
+
