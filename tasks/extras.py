7a2f8224fa272b0b0cb48194bd27835faad3b802
medpaf <pafmed@outlook.com>
2021-10-02 20:04:20 +0100

    small updates and extra to tasks folder

---
+++ tasks/extras.py
@@ -0,0 +1,21 @@
+import time
+from colorama import Fore, Back, Style
+
+def printcolor(color, text):
+    if color.upper() == 'RED':
+        print(f'{Fore.RED}' + text + Style.RESET_ALL)
+    elif color.upper() == 'GREEN':
+        print(f'{Fore.GREEN}' + text + Style.RESET_ALL)
+    elif color.upper() == 'BLUE':
+        print(f'{Fore.BLUE}' + text + Style.RESET_ALL)
+    elif color.upper() == 'YELLOW':
+        print(f'{Fore.YELLOW}' + text + Style.RESET_ALL)
+
+def inputcolor(color, text):
+    if color.upper() == 'RED':
+        comm = input(f'{Fore.RED}' + text + Style.RESET_ALL)
+    elif color.upper() == 'GREEN':
+        comm = input(f'{Fore.GREEN}' + text + Style.RESET_ALL)
+    elif color.upper() == 'BLUE':
+        comm = input(f'{Fore.BLUE}' + text + Style.RESET_ALL)
+    return comm
