b2de05604e09038df942da7006f45a84e079f602
medpaf <pafmed@outlook.com>
2021-10-26 14:49:35 +0100

    MU: check if domain is registered before attempting WHOIS

--- tasks/whoisinfo.py
+++ tasks/whoisinfo.py
@@ -1,55 +1,60 @@
 import whois
+import sys
 from colorama import Fore, Back, Style
 
 def whoisinfo(host):
     
-    try:
-        whois_info = whois.whois(host)
-    except Exception as e:
-        print(f'[{Fore.RED}!{Style.RESET_ALL}] Error: {e}')
+    if not is_registered(host):
+        print(f'[{Fore.RED}!{Style.RESET_ALL}] {Fore.YELLOW}{host}{Style.RESET_ALL} {Fore.RED}is not registered.{Style.RESET_ALL}')
+        sys.exit()
     else:
-        if isinstance(whois_info.domain_name, str):
-            print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Domain name: {Fore.GREEN}{whois_info.domain_name}{Style.RESET_ALL}')
+        try:
+            whois_info = whois.whois(host)
+        except Exception as e:
+            print(f'[{Fore.RED}!{Style.RESET_ALL}] Error: {e}')
         else:
-            print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Domain names')
-            whois_checker(host, whois_info.domain_name)
+            if isinstance(whois_info.domain_name, str):
+                print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Domain name: {Fore.GREEN}{whois_info.domain_name}{Style.RESET_ALL}')
+            else:
+                print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Domain names')
+                whois_checker(host, whois_info.domain_name)
 
-        print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Registar: {Fore.GREEN}{whois_info.registrar}{Style.RESET_ALL}')
-        print(f'[{Fore.GREEN}+{Style.RESET_ALL}] WHOIS server: {Fore.GREEN}{whois_info.whois_server}{Style.RESET_ALL}')
+            print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Registar: {Fore.GREEN}{whois_info.registrar}{Style.RESET_ALL}')
+            print(f'[{Fore.GREEN}+{Style.RESET_ALL}] WHOIS server: {Fore.GREEN}{whois_info.whois_server}{Style.RESET_ALL}')
 
-        print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Name servers')
-        whois_checker(host, whois_info.name_servers)
+            print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Name servers')
+            whois_checker(host, whois_info.name_servers)
 
-        if isinstance(whois_info.creation_date, list):
-            print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Creation dates')
-            whois_checker(host, whois_info.creation_date)
-        else:
-            print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Creation date: {Fore.GREEN}{whois_info.creation_date}{Style.RESET_ALL}')
-            
-        if isinstance(whois_info.updated_date, list):
-            print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Updated dates')
-            whois_checker(host, whois_info.updated_date)
-        else:
-            print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Updated date: {Fore.GREEN}{whois_info.updated_date}{Style.RESET_ALL}')
+            if isinstance(whois_info.creation_date, list):
+                print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Creation dates')
+                whois_checker(host, whois_info.creation_date)
+            else:
+                print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Creation date: {Fore.GREEN}{whois_info.creation_date}{Style.RESET_ALL}')
+                
+            if isinstance(whois_info.updated_date, list):
+                print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Updated dates')
+                whois_checker(host, whois_info.updated_date)
+            else:
+                print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Updated date: {Fore.GREEN}{whois_info.updated_date}{Style.RESET_ALL}')
 
-        if isinstance(whois_info.expiration_date, list):
-            print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Expiration dates')
-            whois_checker(host, whois_info.expiration_date)
-        else:
-            print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Expiration date: {Fore.GREEN}{whois_info.expiration_date}{Style.RESET_ALL}')
+            if isinstance(whois_info.expiration_date, list):
+                print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Expiration dates')
+                whois_checker(host, whois_info.expiration_date)
+            else:
+                print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Expiration date: {Fore.GREEN}{whois_info.expiration_date}{Style.RESET_ALL}')
 
-        print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Status')
-        whois_checker(host, whois_info.status)
+            print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Status')
+            whois_checker(host, whois_info.status)
 
-        if isinstance(whois_info.emails, list):
-            print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Emails')
-            whois_checker(host, whois_info.emails)
-        else:
-            print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Email: {Fore.GREEN}{whois_info.emails}{Style.RESET_ALL}')
+            if isinstance(whois_info.emails, list):
+                print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Emails')
+                whois_checker(host, whois_info.emails)
+            else:
+                print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Email: {Fore.GREEN}{whois_info.emails}{Style.RESET_ALL}')
 
-        print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Organization: {Fore.GREEN}{whois_info.org}{Style.RESET_ALL}')
-        print(f'[{Fore.GREEN}+{Style.RESET_ALL}] State: {Fore.GREEN}{whois_info.state}{Style.RESET_ALL}')
-        print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Country: {Fore.GREEN}{whois_info.country}{Style.RESET_ALL}')
+            print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Organization: {Fore.GREEN}{whois_info.org}{Style.RESET_ALL}')
+            print(f'[{Fore.GREEN}+{Style.RESET_ALL}] State: {Fore.GREEN}{whois_info.state}{Style.RESET_ALL}')
+            print(f'[{Fore.GREEN}+{Style.RESET_ALL}] Country: {Fore.GREEN}{whois_info.country}{Style.RESET_ALL}')
 
 def whois_checker(host, dictionary):
 
@@ -60,3 +65,12 @@ def whois_checker(host, dictionary):
     else: 
         for value in dictionary:
             print(f'\t{Fore.GREEN}{value}{Style.RESET_ALL}')
+
+def is_registered(host):
+
+    try:
+        check = whois.whois(host)
+    except Exception:
+        return False
+    else:
+        return bool(check.host)
