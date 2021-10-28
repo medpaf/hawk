37abd715b5b685b113309677669be97299f96093
medpaf <pafmed@outlook.com>
2021-10-28 13:00:37 +0100

    SU: password wordlist

--- files/conf.py
+++ files/conf.py
@@ -1,3 +1,7 @@
+'''
+This is the MedSec configuration file. Change the configuration based on your specific needs.
+'''
+
 # API KEYS
 SHODAN_API_KEY = 'Q4wtGdNmqnaQ267zzUb2rQztDRayEISI'
 IPINFO_API_KEY = '8d53c2357c2a13'
@@ -5,3 +9,6 @@ IPINFO_API_KEY = '8d53c2357c2a13'
 # DEFAULT WORDLISTS
 PASSWORDS_WORDLIST = 'files/txt/passwords.txt'
 SUBDOMAINS_WORDLIST = 'files/txt/subdomains.txt'
+
+# DEFAULT WIRELESS INTERFACE
+DEFAULT_WIRELESS_INTERFACE = ''
