478d0815a8e89f340d7c99313e9f4abe9f45a0bb
medpaf <pafmed@outlook.com>
2021-10-30 18:15:27 +0100

    SU

--- welcome.py
+++ welcome.py
@@ -3,10 +3,9 @@ from colorama import Fore, Back, Style
 from extras import printcolor
 
 def intro1():
-    printcolor('RED','                                                               ')   
-    printcolor('RED','                        `/-`                                         ')
+    printcolor('RED','\n                        `/-`                                       ')
     printcolor('RED','                         :/:.`                                       ')
-    printcolor('RED','                         `///:.`..-::::////::::-..`                  ')
+    printcolor('RED','                         `///:.`..-::::////::::-..                   ')
     printcolor('RED','                          .://////////////////////::-``          ..  ')
     printcolor('RED','                           `-/////////////////////////:.`        ::` ')
     printcolor('RED','                       `.`   `-:////////////////////////:.`     `//` ')
@@ -27,21 +26,8 @@ def intro1():
     printcolor('RED','                       `-///////////:   `-///////////////:`          ')
     printcolor('RED','                         .-/////////.``-://////////////:.`           ')
     printcolor('RED','                           `.://///..-//////////////:-`              ')
-    printcolor('RED','                             `.-:://///////////:-.`   v              ')
-    printcolor('RED','                                 ``........``                        ')
-
-def intro2():
-    printcolor('RED', '\n    _____________  _______   ________  ________  _______   ________      ')
-    printcolor('RED', '   |\   ___\__   \|\  ___ \ |\   ___ \|\   ____\|\  ___ \ |\   ____\     ')
-    printcolor('RED', '   \ \  \ \__\ \  \ \   __/|\ \  \__\ \ \  \___|\ \   __/|\ \  \___|     ')
-    printcolor('RED', '    \ \  \ |__| \  \ \  \_|/_\ \  \  \ \ \_____  \ \  \_|/_\ \  \        ')
-    printcolor('RED', '     \ \  \    \ \  \ \  \_|\ \ \  \__\ \|____|\  \ \  \_|\ \ \  \_____  ')
-    printcolor('RED', '      \ \__\    \ \__\ \_______\ \_______\____\_\  \ \_______\ \_______\ ')
-    printcolor('RED', '       \|__|     \|__|\|_______|\|_______|\_________\|_______|\|_______| ')
-    printcolor('RED', '                                         \|_________| \n                  ')                                                         
-    print(f'                        Written by {Fore.RED}Paulo Medeiros{Style.RESET_ALL}              ')
-    print(f'             Github repository: {Fore.RED}https://github.com/medpaf/medsec{Style.RESET_ALL}          ')
-                                                                                                                                          
+    printcolor('RED','                             `.-:://///////////:-.`                  ')
+    printcolor('RED','                                 ``........``                        ')                                                                                                                             
 
 def welcome():
     introList=[intro1]
